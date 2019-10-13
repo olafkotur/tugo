ef5c280d24554dce7aceae7107dfc9e47d2ca5e5
olafkotur <olafkotur97@gmail.com>
2019-10-13 17:50:06 +0100

    Fixed dotenv and added slack service

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { SlackService } from './src/server/services/slack';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity
+        onPress={ () => SlackService.sendMessage("Hello I'm Juno, this is my first ever message! I hope that I will be able to help out here and there :)", 'report')}>
+        <Text>Press meeeee</Text>
+      </TouchableOpacity>
     </View>
   );
 }

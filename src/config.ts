393a949b8bac8af5873ff5157398fa9ddf8e22ba
olafkotur <olafkotur97@gmail.com>
2019-10-13 16:56:41 +0100

    Added slack hooks

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,21 @@
-import { FIREBASE_API_KEY } from 'react-native-dotenv';
+import { FIREBASE_API_KEY, SLACK_HOOK_REPORTING, SLACK_HOOK_LOG } from 'react-native-dotenv';
 
 interface IConfig {
   firebase: { apiKey: string };
+  slack: {
+    logChannel: string;
+    reportChannel: string;
+    logHook: string;
+    reportHook: string,
+  };
 }
 
 export const config: IConfig = {
   firebase: { apiKey: FIREBASE_API_KEY || '' },
+  slack: {
+    logChannel: '#dev_logs',
+    reportChannel: '#dev_reporting',
+    logHook: SLACK_HOOK_LOG || '',
+    reportHook: SLACK_HOOK_REPORTING || '',
+  },
 };
